2f983c542ff3a85796172ec5e29788b5c1845d6c
Daniel Caita <dacaitac@unal.edu.co>
2018-08-04 14:22:52 -0500

    - Update Methods Setup inside Index

--- index.js
+++ index.js
@@ -1,9 +1,43 @@
-const podio = require('./setup')
+const PodioJS = require('podio-js').api
 const fs = require('fs')
 const config = JSON.parse(fs.readFileSync('./config.json'))
 const converter = require('./converter')
 
-const itemId = 902773349 // Just for testing
+// Just for testing
+const itemId = 902773349
+
+// get the API id/secret
+const clientId = config.clientId
+const clientSecret = config.clientSecret
+
+// get the app ID and Token for appAuthentication
+const appId = config.appId
+const appToken = config.appToken
+
+// instantiate the SDK
+const podio = new PodioJS({
+  authType: 'server',
+  clientId: clientId,
+  clientSecret: clientSecret
+})
+
+function request (method, podioRequest, data, callback) {
+  data = data || null
+  podio.authenticateWithApp(appId, appToken, (err) => {
+    if (err) throw new Error(err)
+
+    podio.isAuthenticated()
+      .then(() => { // Ready to make API calls in here...
+        console.log('Making Request')
+        podio.request(method, podioRequest, data)
+          .then(response => {
+            console.log('Request Complete')
+            callback(response)
+          })
+          .catch(err => console.log(err))
+      }).catch(err => console.log(err))
+  })
+}
 
 function setPodioObject (response) { // Este ojeto se define para cada instancia del programa
   return {
@@ -15,7 +49,7 @@ function setPodioObject (response) { // Este ojeto se define para cada instancia
 }
 
 function getItems (appId) {
-  podio.request('GET', `/item/app/${appId}/`, (response) => {
+  request('GET', `/item/app/${appId}/`, null,(response) => {
     let itemList = response.items
     itemList.map(function (item) {
       item = setPodioObject(item)
@@ -25,15 +59,31 @@ function getItems (appId) {
 }
 
 function itemAction (itemId) {
-  podio.request('GET', `/item/${itemId}/`, null, (item) => {
-    let podioObject = setPodioObject(item)
+  request('GET', `/item/${itemId}`, null, (response) => {
+    let podioObject = setPodioObject(response)
     converter.convertPDF(config, podioObject)
-      .then((pdf) => {
-        podio.uploadFile(pdf)
+      .then( (pdf) => {
+        let str = pdf.filename.split('/')
+        let data = {
+          filename: str[str.length - 1],
+          filepath: pdf.filename
+        }
+        return data
+      })
+      .then( (data) => {
+        // Sube el archivo y retorna el id
+        console.log('Uploading File');
+        return podio.uploadFile(data.filepath, data.filename)
+      })
+      .then ((podioFile) => {
+        let fileId = podioFile.file_id
+        console.log(`File uploaded FileID: ${fileId}`);
+        request('POST', `/file/${fileId}/attach`, {file_id: fileId, ref_type: 'item', ref_id: itemId},
+          () => { console.log('File attached') })
       })
       .catch(err => console.log(err))
   })
 }
 
-itemAction(itemId)
-// getItems(config.appId)
+// itemAction(itemId)
+getItems(config.appId)
